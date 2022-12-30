19d595498aeb67ffead53f81d84fa4ea9f859858
Vishu <vs1015242@gmail.com>
2022-12-30 16:08:06 +0530

    remove

--- mysite/urls.py
+++ mysite/urls.py
@@ -36,7 +36,6 @@ urlpatterns += [
     # ... the rest of your URLconf goes here ...
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += [
 #     # ... the rest of your URLconf goes here ...
 # ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
